e9d5ea177ad27f0220ca69379bf1597d799122af
Stéphane Brunner <stephane.brunner@camptocamp.com>
2014-02-10 11:54:32 +0100

    Update to 1.4.2

--- demo/static/mobile/app/controller/Query.js
+++ demo/static/mobile/app/controller/Query.js
@@ -27,7 +27,7 @@ Ext.define('App.controller.Query', {
                 autoCreate: true
             },
             queryViewList: {
-                selector: 'queryview list'
+                selector: 'queryview'
             }
         },
         control: {
@@ -38,7 +38,7 @@ Ext.define('App.controller.Query', {
             }
         },
         routes: {
-            'query/:coords': {
+            'query/:coords/:layers': {
                 action: 'showQueryResultView',
                 condition: '.+'
             }
@@ -58,23 +58,23 @@ Ext.define('App.controller.Query', {
         });
     },
 
-    showQueryResultView: function(params) {
+    showQueryResultView: function(coords, layers) {
         var store = this.getQueryView().getStore();
         store.removeAll();
 
-        params = decodeURIComponent(params);
-        params = params.split('-');
-        var bounds = params[0].split(',');
+        layers = decodeURIComponent(layers).split('-');
 
+        bounds = coords.split('-');
         bounds = new OpenLayers.Bounds.fromArray(bounds);
         var filter = new OpenLayers.Filter.Spatial({
             type: OpenLayers.Filter.Spatial.BBOX,
             value: bounds
         });
-        this.protocol.format.featureType = params[1].split(',');
+        this.protocol.format.featureType = layers;
         var response = this.protocol.read({
             maxFeatures: 20,
             filter: filter,
+            params: this.getApplication().getController('Main').getParams(),
             callback: function(result) {
                 if(result.success()) {
                     if(result.features.length) {
