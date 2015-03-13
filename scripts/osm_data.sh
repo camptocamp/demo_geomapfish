25bc26f1cf9f78eb58a8e61a4594db7fb97d8a4b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-03-13 10:52:57 +0100

    Add index

--- scripts/osm_data.sh
+++ scripts/osm_data.sh
@@ -2,4 +2,8 @@
 
 wget http://download.geofabrik.de/europe/switzerland-latest.osm.pbf
 
-sudo -u postgres osm2pgsql -s --proj 21781 -d osm ./switzerland-latest.osm.pbf
+sudo -u postgres osm2pgsql --slim --proj 21781 ---database osm ./switzerland-latest.osm.pbf
+
+sudo -u postgres psql -c "CREATE INDEX ON planet_osm_point (amenity);" osm
+sudo -u postgres psql -c "CREATE INDEX ON planet_osm_point (tourism);" osm
+sudo -u postgres psql -c "CREATE INDEX ON planet_osm_point (highway);" osm
