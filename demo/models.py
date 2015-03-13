c2feec59a39ae23a403eb1d34b7a8f2f36606a8b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-03-13 15:05:38 +0100

    Hide unwanted table

--- demo/models.py
+++ demo/models.py
@@ -2,11 +2,16 @@
 
 import logging
 
-from sqlalchemy import Column, types
+from pyramid.security import DENY_ALL
 from pyramid.i18n import TranslationStringFactory
 
-from c2cgeoportal.models import *
-#from c2cgeoportal.models import _schema
+from c2cgeoportal.models import *  # noqa
+# from c2cgeoportal.models import _schema
 
 _ = TranslationStringFactory('demo')
 log = logging.getLogger(__name__)
+
+LayerInternalWMS.__acl__ = [DENY_ALL]
+LayerExternalWMS.__acl__ = [DENY_ALL]
+LayerWMTS.__acl__ = [DENY_ALL]
+WMTSDimension.__acl__ = [DENY_ALL]
