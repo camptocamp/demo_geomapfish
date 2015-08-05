9a72e3dfe8cc7f0be4ae6af8e17513f56592c056
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-08-05 09:02:00 +0200

    Create the new demo site

--- demo.mk
+++ demo.mk
@@ -1,15 +1,11 @@
 ifdef VARS_FILE
-VARS_FILES += ${VARS_FILE} vars_geoportailv3.yaml
+VARS_FILES += ${VARS_FILE} vars_demo.yaml
 else
 VARS_FILE = vars_demo.yaml
 VARS_FILES += ${VARS_FILE}
 endif
 
-INSTANCE_ID ?= 1.6
-PRINT_VERSION ?= 3
+INSTANCE_ID ?= 2.0
 APACHE_VHOST ?= geomapfish-demo
-
-PRINT_INPUT += WEB-INF
-PRINT_REQUIREMENT += print/WEB-INF/classes/mapfish-spring-application-context-override.xml
 
 include CONST_Makefile
