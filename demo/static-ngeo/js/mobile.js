3f371aa57ee3f34294fe49f7c7d23f5a914671aa
marionb <marion.baumgartner@camptocamp.com>
2016-01-27 14:00:27 +0100

    upgrade revision 2.0.0dev1

--- demo/static-ngeo/js/mobile.js
+++ demo/static-ngeo/js/mobile.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview Application entry point.
+ * Application entry point.
  *
  * This file defines the "demo_mobile" Closure namespace, which is be used as the
  * Closure entry point (see "closure_entry_point" in the "build.json" file).
@@ -18,37 +18,34 @@ goog.require('gmf.authenticationDirective');
 /** @suppress {extraRequire} */
 goog.require('gmf.proj.EPSG21781');
 /** @suppress {extraRequire} */
-goog.require('gmf.searchDirective');
-/** @suppress {extraRequire} */
 goog.require('ngeo.mobileGeolocationDirective');
-
-
-demoModule.constant(
-    'authenticationBaseUrl',
-    'https://geomapfish-demo.camptocamp.net/2.0/wsgi');
 
 
 
 /**
+ * @param {string} defaultLang The default language.
+ * @param {Object.<string, string>} langUrls The languages URLs.
+ * @param {angularGettext.Catalog} gettextCatalog Gettext catalog.
+ * @param {ngeo.GetBrowserLanguage} ngeoGetBrowserLanguage
+ * @param {angular.Scope} $scope Scope.
+ * @param {ngeo.StateManager} ngeoStateManager the state manager.
  * @param {ngeo.FeatureOverlayMgr} ngeoFeatureOverlayMgr The ngeo feature
  *     overlay manager service.
- * @param {Object} serverVars vars from GMF
- * @param {angularGettext.Catalog} gettextCatalog Gettext catalog.
- * @param {ngeo.StateManager} ngeoStateManager the state manager.
- * @param {angular.Scope} $scope Scope.
- * @param {ngeo.GetBrowserLanguage} ngeoGetBrowserLanguage
  * @param {gmf.Themes} gmfThemes Themes service.
+ * @param {string} fulltextsearchUrl url to a gmf fulltextsearch service.
  * @constructor
  * @extends {gmf.AbstractMobileController}
  * @ngInject
  * @export
  */
 demo.MobileController = function(
-    ngeoFeatureOverlayMgr, serverVars, gettextCatalog, ngeoStateManager, $scope,
-    ngeoGetBrowserLanguage, gmfThemes) {
+    defaultLang, langUrls, gettextCatalog, ngeoGetBrowserLanguage,
+    $scope, ngeoStateManager, ngeoFeatureOverlayMgr,
+    gmfThemes, fulltextsearchUrl) {
   goog.base(
-      this, ngeoFeatureOverlayMgr, serverVars, gettextCatalog, ngeoStateManager,
-      $scope, ngeoGetBrowserLanguage, gmfThemes);
+      this, defaultLang, langUrls, gettextCatalog, ngeoGetBrowserLanguage,
+      $scope, ngeoStateManager, ngeoFeatureOverlayMgr, 21781,
+      gmfThemes, fulltextsearchUrl);
 };
 goog.inherits(demo.MobileController, gmf.AbstractMobileController);
 
