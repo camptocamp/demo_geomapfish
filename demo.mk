8272a2ee57d57bfb9af120194ba5836386f77885
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-05-23 15:51:20 +0200

    Update for test server

--- demo.mk
+++ demo.mk
@@ -5,9 +5,8 @@ VARS_FILE = vars_demo.yaml
 VARS_FILES += ${VARS_FILE}
 endif
 
-INSTANCE_ID ?= 1.6
 PRINT_VERSION ?= 3
-APACHE_VHOST ?= geomapfish-demo
+APACHE_VHOST ?= gmfusrgrp_version2-geomapfishtest
 
 PRINT_INPUT += WEB-INF
 PRINT_REQUIREMENT += print/WEB-INF/classes/mapfish-spring-application-context-override.xml
---
+++ demo_server.mk
@@ -0,0 +1,4 @@
+INSTANCE_ID = 1.6
+APACHE_VHOST = geomapfish-demo
+
+include demo.mk
