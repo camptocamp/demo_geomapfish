7d416172c49ea90b7ac8ce475c2e856e2bd99e66
ger-benjamin <benjamin.gerber@camptocamp.com>
2017-01-18 15:25:01 +0100

    Better vars file

--- bge.mk
+++ bge.mk
@@ -1,15 +1,4 @@
-ifdef VARS_FILE
-VARS_FILES += ${VARS_FILE} vars_geoportailv3.yaml
-else
-VARS_FILE = vars_demo.yaml
-VARS_FILES += ${VARS_FILE}
-endif
+INSTANCE_ID = bge
+DEVELOPMENT = TRUE
 
-INSTANCE_ID ?= bge
-PRINT_VERSION ?= 3
-APACHE_VHOST ?= geomapfish-demo
-
-PRINT_INPUT += WEB-INF
-PRINT_REQUIREMENT += print/WEB-INF/classes/mapfish-spring-application-context-override.xml
-
-include CONST_Makefile
+include demo.mk
