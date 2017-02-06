be42be5f0f7865c6e06204ad7edf03014b76e88c
ger-benjamin <benjamin.gerber@camptocamp.com>
2017-02-06 16:25:13 +0100

    Some config for v2.2

--- bge.mk
+++ bge.mk
@@ -1,15 +1,5 @@
-ifdef VARS_FILE
-VARS_FILES += ${VARS_FILE} vars_geoportailv3.yaml
-else
-VARS_FILE = vars_demo.yaml
-VARS_FILES += ${VARS_FILE}
-endif
+INSTANCE_ID = bge
+DEVELOPMENT = TRUE
+#REQUIREMENTS = -e ../c2cgeoportal
 
-INSTANCE_ID ?= bge
-PRINT_VERSION ?= 3
-APACHE_VHOST ?= geomapfish-demo
-
-PRINT_INPUT += WEB-INF
-PRINT_REQUIREMENT += print/WEB-INF/classes/mapfish-spring-application-context-override.xml
-
-include CONST_Makefile
+include demo.mk
