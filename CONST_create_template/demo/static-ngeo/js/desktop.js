9e8be48f64babfa49565c29629f0d579d32f26b1
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-05-03 20:57:54 +0200

    Upgrade to GeoMapFish 2.2.0.dev10

--- CONST_create_template/demo/static-ngeo/js/desktop.js
+++ CONST_create_template/demo/static-ngeo/js/desktop.js
@@ -14,6 +14,8 @@ goog.provide('demo_desktop');
 goog.require('demo');
 goog.require('gmf.AbstractDesktopController');
 /** @suppress {extraRequire} */
+goog.require('ngeo.googlestreetviewComponent');
+/** @suppress {extraRequire} */
 goog.require('ngeo.proj.EPSG2056');
 /** @suppress {extraRequire} */
 goog.require('ngeo.proj.EPSG21781');
