e0e2763e62928495c62fad61f75409c4124d3f15
Stéphane Brunner <stephane.brunner@camptocamp.com>
2017-05-04 10:26:04 +0200

    No more needed

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm.session import sessionmaker
-import sqlahelper
 from pyramid.config import Configurator
 from c2cgeoportal.pyramid_ import locale_negotiator, add_interface, INTERFACE_TYPE_NGEO
 from c2cgeoportal.lib.authentication import create_authentication
@@ -21,12 +18,6 @@ def main(global_config, **settings):
     config.include("c2cgeoportal")
 
     config.add_translation_dirs("demo:locale/")
-
-    osm_engine = create_engine(config.get_settings().get("dbsessions").get("osm").get("url"))
-    sqlahelper.add_engine(osm_engine, "osm")
-    osm_session = sessionmaker()
-    from c2cgeoportal.models import DBSessions
-    DBSessions["osm"] = osm_session(bind=osm_engine)
 
     # scan view decorator for adding routes
     config.scan()
