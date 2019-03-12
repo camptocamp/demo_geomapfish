c4ce0726632326528e1153f85c0b622f7ddf25ab
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-03-12 16:49:48 +0100

    Set the right URL in the API

--- geoportal/demo_geoportal/static-ngeo/api/index.js
+++ geoportal/demo_geoportal/static-ngeo/api/index.js
@@ -1,7 +1,7 @@
 import Map from 'api/Map.js';
 import * as constants from 'api/constants.js';
 
-constants.themesUrl = 'https://www.example.com';
+constants.themesUrl = 'https://geomapfish-demo-2-4.camptocamp.com/themes?version=2&background=background';
 
 const lib = {
   Map
