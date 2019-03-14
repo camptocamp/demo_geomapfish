87a4ca5f17944cff0f3249da356a53164d72e191
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-03-14 15:49:49 +0000

    Upgrade to GeoMapFish 2.4

--- geoportal/webpack.config.js
+++ geoportal/webpack.config.js
@@ -1,6 +1,6 @@
 const webpackMerge = require('webpack-merge');
 const apps = require('./webpack.apps.js');
-const commons = require('./webpack.commons.js');
+const commons = require('./webpack.commons');
 
 let config = commons.config;
 
