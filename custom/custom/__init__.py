2c7a518d87e031f6f79908e69be59dafcafd55b4
Stéphane Brunner <stephane.brunner@camptocamp.com>
2021-11-04 11:52:46 +0100

    Add Prospector

--- custom/custom/__init__.py
+++ custom/custom/__init__.py
@@ -3,6 +3,7 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     """This function returns a Pyramid WSGI application."""
+    del global_config  # Unused.
     with Configurator(settings=settings) as config:
         config.include("pyramid_mako")
         config.include("cornice")
