a2b1b5ab1bb2cab324e080877fa3926df98eaf61
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2021-11-16 11:33:39 +0100

    better file naming

--- geoportal/geomapfish_geoportal/static-ngeo/js/apps/Controllerdesktop_alt.js
+++ geoportal/geomapfish_geoportal/static-ngeo/js/apps/Controllerdesktop_alt.js
@@ -42,7 +42,7 @@ import ngeoStreetviewModule from 'ngeo/streetview/module';
 import ngeoRoutingModule from 'ngeo/routing/module';
 import ngeoStatemanagerWfsPermalink from 'ngeo/statemanager/WfsPermalink';
 
-import '../customization_loader';
+import '../customizationLoader';
 
 /**
  * @private
