c301cee35e48326006df4f5a7e9d806f6cdcc855
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2021-11-16 15:24:36 +0100

    Add a new (auth) component, inherited from another

---
+++ geoportal/geomapfish_geoportal/static-ngeo/js/customization/panelElement.ts
@@ -0,0 +1,66 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2021 Camptocamp SA
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import {html, TemplateResult, unsafeCSS} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {unsafeSVG} from 'lit/directives/unsafe-svg.js';
+import loadingSvg from 'gmf/icons/spinner.svg';
+import i18next from 'i18next';
+import 'ngeo/auth/FormElement';
+
+import GmfAuthPanel from 'ngeo/auth/PanelElement';
+
+// CUSTOM element that inherit from GmfAuthPanel.
+@customElement('demo-auth-panel')
+export default class DemoAuthPanel extends GmfAuthPanel {
+  // CUSTOM property
+  @property({type: String}) customMessage: string = 'Not set';
+
+  protected render(): TemplateResult {
+    const spinnerTemplate = this.postLoading
+      ? html`
+          <div>
+            <i class="fa fa-spin svg-spinner"
+              >${
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                unsafeSVG(loadingSvg)
+              }</i
+            >
+            ${i18next.t('Loading themes, please wait...')}
+          </div>
+        `
+      : '';
+    return html`
+      <style>
+        ${unsafeCSS(this.customCSS_)}
+      </style>
+      <!-- CUSTOM-->
+      <p>With my custom message: ${this.customMessage}</p>
+      <!-- CUSTOM END-->
+      ${this.getTitle(i18next.t('Login'))}
+      <gmf-auth-form
+        .loginInfoMessage=${this.loginInfoMessage}
+        .passwordValidator=${this.passwordValidator}
+      ></gmf-auth-form>
+      ${spinnerTemplate}
+    `;
+  }
+}
--- geoportal/geomapfish_geoportal/static-ngeo/js/customizationLoader.ts
+++ geoportal/geomapfish_geoportal/static-ngeo/js/customizationLoader.ts
@@ -2,5 +2,6 @@
 import './customization/user.ts';
 import './customization/authService.ts';
 import './customization/authFormElement.ts';
+import './customization/panelElement.ts';
 // Small test to override an AngularJS component.
 import './customization/angularDrawPanel.ts';
