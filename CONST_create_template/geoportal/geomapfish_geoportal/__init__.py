e17dafeef1b05455c02a80c1ef85ca4da23a901b
Continuous integration <ci@camptocamp.com>
2021-12-03 14:55:30 +0000

    Upgrade to master

--- CONST_create_template/geoportal/geomapfish_geoportal/__init__.py
+++ CONST_create_template/geoportal/geomapfish_geoportal/__init__.py
@@ -1,10 +1,11 @@
 import distutils.core
 
-from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.config import Configurator
 
+import geomapfish_geoportal.authentication
+import geomapfish_geoportal.dev
+import geomapfish_geoportal.multi_organization
 from c2cgeoportal_geoportal import INTERFACE_TYPE_NGEO, add_interface_config, locale_negotiator
-from c2cgeoportal_geoportal.lib.authentication import create_authentication
 from c2cgeoportal_geoportal.lib.i18n import LOCALE_PATH
 from geomapfish_geoportal.resources import Root
 
@@ -23,8 +24,7 @@ def main(global_config, **settings):
 
     config.include("c2cgeoportal_commons")
 
-    config.set_authorization_policy(ACLAuthorizationPolicy())
-    config.set_authentication_policy(create_authentication(config.get_settings()))
+    config.include(geomapfish_geoportal.authentication.includeme)
 
     config.add_translation_dirs(LOCALE_PATH)
 
@@ -33,6 +33,8 @@ def main(global_config, **settings):
     config.include("c2cgeoportal_geoportal")
     distutils.core._setup_stop_after = None  # pylint: disable=protected-access
 
+    config.include(geomapfish_geoportal.multi_organization.includeme)
+
     # Scan view decorator for adding routes
     config.scan()
 
@@ -40,12 +42,6 @@ def main(global_config, **settings):
     for interface in config.get_settings().get("interfaces", []):
         add_interface_config(config, interface)
 
-    try:
-        import ptvsd  # pylint: disable=import-error,import-outside-toplevel
-
-        ptvsd.enable_attach(address=("172.17.0.1", 5678))
-        # ptvsd.wait_for_attach()
-    except ModuleNotFoundError:
-        pass
+    config.include(geomapfish_geoportal.dev.includeme)
 
     return config.make_wsgi_app()
