8dbd9d6a9e02a8496382826bc515a7c5c9c96601
Stéphane Brunner <stephane.brunner@camptocamp.com>
2022-02-08 13:24:26 +0100

    Use c2cwsgiutils

--- custom/custom/__init__.py
+++ custom/custom/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
-
+import c2cwsgiutils.health_check
+import c2cwsgiutils.db
 
 def main(global_config, **settings):
     """This function returns a Pyramid WSGI application."""
@@ -8,6 +9,10 @@ def main(global_config, **settings):
         config.include("pyramid_mako")
         config.include("cornice")
         config.include(".routes")
-        config.include(".models")
+        config.include("c2cwsgiutils.pyramid")
+        dbsession = c2cwsgiutils.db.init(config, "sqlalchemy", "sqlalchemy_slave")
         config.scan()
+        # Initialize the health checks
+        health_check = c2cwsgiutils.health_check.HealthCheck(config)
+        health_check.add_db_session_check(dbsession)
     return config.make_wsgi_app()
