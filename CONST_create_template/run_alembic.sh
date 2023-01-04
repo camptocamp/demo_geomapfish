64d3d79c27b45142c731141b990efc8a4f7c934f
Continuous integration <ci@camptocamp.com>
2023-01-04 12:20:02 +0000

    Interrupted upgrade to master

--- CONST_create_template/run_alembic.sh
+++ CONST_create_template/run_alembic.sh
@@ -3,8 +3,10 @@
 #
 # Mostly useful in a Docker environment.
 
-for ini in *alembic*.ini; do
-    if [[ -f $ini ]]; then
+for ini in *alembic*.ini
+do
+    if [[ -f $ini ]]
+    then
         echo "$ini ==========================="
         alembic -c $ini upgrade head
     fi
