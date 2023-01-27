dfcffbe9c296fb6c3a71dfa335c23d92ea109de5
Continuous integration <ci@camptocamp.com>
2023-01-27 13:45:52 +0000

    Interrupted upgrade to latest

--- geoportal/webpack.apps.js
+++ geoportal/webpack.apps.js
@@ -21,7 +21,7 @@ for (const filename of ls(path.resolve(__dirname, 'geomapfish_geoportal/static-n
       chunks: [name],
       vars: {
         entry_point: '${VISIBLE_ENTRY_POINT}',
-        version: 'latest',
+        version: '2.7.0.173',
         cache_version: '${CACHE_VERSION}',
       },
     })
