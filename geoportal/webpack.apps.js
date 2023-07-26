162412ac7d3dc48577e720cb1b58e7973841a144
Continuous integration <ci@camptocamp.com>
2023-07-26 11:25:01 +0000

    Upgrade to 2.6.0.203

--- geoportal/webpack.apps.js
+++ geoportal/webpack.apps.js
@@ -21,7 +21,7 @@ for (const filename of ls(path.resolve(__dirname, 'geomapfish_geoportal/static-n
       chunks: [name],
       vars: {
         entry_point: '${VISIBLE_ENTRY_POINT}',
-        version: '2.6.0.202',
+        version: '2.6.0.203',
         cache_version: '${CACHE_VERSION}',
        },
     })
