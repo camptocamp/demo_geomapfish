eb24f4f7e18c7a5085eb6f80098d54c776539fc2
Continuous integration <ci@camptocamp.com>
2023-11-01 20:05:10 +0000

    Upgrade to 2.7.1.7

--- CONST_create_template/geoportal/webpack.apps.js
+++ CONST_create_template/geoportal/webpack.apps.js
@@ -21,7 +21,7 @@ for (const filename of ls(path.resolve(__dirname, 'geomapfish_geoportal/static-n
       chunks: [name],
       vars: {
         entry_point: '${VISIBLE_ENTRY_POINT}',
-        version: '2.7.1.6',
+        version: '2.7.1.7',
         cache_version: '${CACHE_VERSION}',
       },
     })
