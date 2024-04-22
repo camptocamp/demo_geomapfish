988033897b5943ff1681264303d2fef8799a0989
Continuous integration <ci@camptocamp.com>
2024-04-22 13:16:29 +0000

    Upgrade to 2.8.1.80

--- geoportal/geomapfish_geoportal/models.py
+++ geoportal/geomapfish_geoportal/models.py
@@ -1,8 +1,7 @@
 import logging
 
-from pyramid.i18n import TranslationStringFactory
-
 from c2cgeoportal_commons.models.main import *  # noqa: ignore=F401, pylint: disable=unused-wildcard-import
+from pyramid.i18n import TranslationStringFactory
 
 _ = TranslationStringFactory("geomapfish_geoportal-server")
 LOG = logging.getLogger(__name__)
