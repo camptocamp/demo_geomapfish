c5fa059b84db0f5c5a7de44f1c9036ff8d963414
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-07-12 08:19:57 +0200

    Fix COG view

--- custom/custom/views/cog.py
+++ custom/custom/views/cog.py
@@ -1,33 +1,12 @@
 import logging
-import logging.config
 import os
-import subprocess  # nosec
-from itertools import product
 
-import boto3
-import botocore.client
-import c2cwsgiutils.pyramid_logging
-import c2cwsgiutils.setup_process
-import jsonschema_validator
-import psycopg2
 import pyramid.request
 import pyramid.response
-import requests
 from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, ContainerClient, ContentSettings
-from c2cwsgiutils import sentry
+from azure.storage.blob import BlobServiceClient, ContainerClient
 from cornice import Service
-from PIL import Image
-from prometheus_client import Counter, Summary
 from pyramid.httpexceptions import HTTPBadRequest
-from ruamel.yaml import YAML
-from shapely.geometry.base import BaseGeometry
-from shapely.geometry.polygon import Polygon
-from shapely.ops import unary_union
-from shapely.wkb import loads as loads_wkb
-
-from custom.models.feedback import Feedback
-from custom.util.send_mail import send_mail
 
 _LOGGING = logging.getLogger(__name__)
 _CLIENT = None
@@ -39,13 +18,13 @@ def _get_azure_container_client(container: str) -> ContainerClient:
         return BlobServiceClient.from_connection_string(
             os.environ["AZURE_STORAGE_CONNECTION_STRING"]
         ).get_container_client(container=container)
-    elif "AZURE_STORAGE_BLOB_CONTAINER_URL" in os.environ:
+    if "AZURE_STORAGE_BLOB_CONTAINER_URL" in os.environ:
         return ContainerClient.from_container_url(os.environ["AZURE_STORAGE_BLOB_CONTAINER_URL"])
-    else:
-        return BlobServiceClient(
-            account_url=os.environ["AZURE_STORAGE_ACCOUNT_URL"],
-            credential=DefaultAzureCredential(),
-        ).get_container_client(container=container)
+
+    return BlobServiceClient(
+        account_url=os.environ["AZURE_STORAGE_ACCOUNT_URL"],
+        credential=DefaultAzureCredential(),
+    ).get_container_client(container=container)
 
 
 feedback = Service(
@@ -63,13 +42,13 @@ def swissalti3d(request: pyramid.request.Request) -> pyramid.response.Response:
     if _CLIENT is None:
         _CLIENT = _get_azure_container_client(os.environ["TILEGENERATION_AZURE_CONTAINER"])
     blob = _CLIENT.get_blob_client(blob="swissalti3d_2m_archeo.tif")
-    range = request.headers.get("Range")
-    if range is None:
+    range_header = request.headers.get("Range")
+    if range_header is None:
         raise HTTPBadRequest("Range header is required")
-    if not range.startsWith("bytes="):
+    if not range_header.startsWith("bytes="):
         raise HTTPBadRequest("Range header must of type bytes")
-    range = range[6:]
-    start_str, end_str = range.split("-")
+    range_header = range_header[6:]
+    start_str, end_str = range_header.split("-")
     start = int(start_str)
     end = int(end_str)
 
