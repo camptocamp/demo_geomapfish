e0b672ace426d605fbabdb987011540dc83b9641
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-10-11 14:01:14 +0200

    Add retry in URL tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 import requests
 
@@ -30,7 +32,12 @@ import requests
 )
 def test_url(url: str, params: dict[str, str], timeout: int) -> None:
     """Tests that some URL didn't return an error."""
-    response = requests.get(url, params=params, verify=False, timeout=timeout)  # nosec
+    response = None
+    for _ in range(5):
+        response = requests.get(url, params=params, verify=False, timeout=timeout)  # nosec
+        if response.status_code != 503:
+            break
+        time.sleep(1)
     assert response.status_code == 200, response.text
 
 
