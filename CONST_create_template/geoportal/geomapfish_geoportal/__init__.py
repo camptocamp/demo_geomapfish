4c8031ac3de7e0bef499134db2f9a54310448f21
Continuous integration <ci@camptocamp.com>
2025-01-31 12:52:22 +0000

    Upgrade to 2.9.rc.62

--- CONST_create_template/geoportal/geomapfish_geoportal/__init__.py
+++ CONST_create_template/geoportal/geomapfish_geoportal/__init__.py
@@ -2,7 +2,6 @@ from pyramid.config import Configurator
 
 import geomapfish_geoportal.authentication
 import geomapfish_geoportal.dev
-import geomapfish_geoportal.multi_organization
 import geomapfish_geoportal.multi_tenant
 from c2cgeoportal_geoportal import add_interface_config, locale_negotiator
 from c2cgeoportal_geoportal.lib.i18n import LOCALE_PATH
@@ -29,7 +28,6 @@ def main(global_config, **settings):
 
     config.include("c2cgeoportal_geoportal")
 
-    config.include(geomapfish_geoportal.multi_organization.includeme)
     config.include(geomapfish_geoportal.multi_tenant.includeme)
 
     # Scan view decorator for adding routes
