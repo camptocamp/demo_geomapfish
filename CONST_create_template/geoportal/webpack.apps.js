65de299475a571ab052675b9a12d245a9791dc41
Continuous integration <ci@camptocamp.com>
2025-12-06 14:13:59 +0000

    Upgrade to 2.9.0.358

--- CONST_create_template/geoportal/webpack.apps.js
+++ CONST_create_template/geoportal/webpack.apps.js
@@ -23,7 +23,7 @@ for (const filename of ls(
       chunks: [name],
       vars: {
         entry_point: '${VISIBLE_ENTRY_POINT}',
-        version: '2.9.0.357',
+        version: '2.9.0.358',
         cache_version: '${CACHE_VERSION}',
       },
     })
