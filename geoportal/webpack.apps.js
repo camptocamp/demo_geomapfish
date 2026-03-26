4825f7b67ef7f238c7b70469d4e7515cb3b4bd6a
Continuous integration <ci@camptocamp.com>
2026-03-26 20:14:42 +0000

    Upgrade to 2.9.0.429

--- geoportal/webpack.apps.js
+++ geoportal/webpack.apps.js
@@ -23,7 +23,7 @@ for (const filename of ls(
       chunks: [name],
       vars: {
         entry_point: '${VISIBLE_ENTRY_POINT}',
-        version: '2.9.0.428',
+        version: '2.9.0.429',
         cache_version: '${CACHE_VERSION}',
       },
     })
