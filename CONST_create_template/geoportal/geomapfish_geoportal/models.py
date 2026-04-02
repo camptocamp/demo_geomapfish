09d88f56da0b4eb9853017bbf4e54461a3bc9578
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-04-02 14:21:14 +0200

    Upgrade to GeoMapFish 2.10

--- CONST_create_template/geoportal/geomapfish_geoportal/models.py
+++ CONST_create_template/geoportal/geomapfish_geoportal/models.py
@@ -1,8 +1,7 @@
 import logging
 
-from pyramid.i18n import TranslationStringFactory
-
 from c2cgeoportal_commons.models.main import *  # pylint: disable=unused-wildcard-import # noqa: F403
+from pyramid.i18n import TranslationStringFactory
 
 _ = TranslationStringFactory("geomapfish_geoportal-server")
 _LOG = logging.getLogger(__name__)
