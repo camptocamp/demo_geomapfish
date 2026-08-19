acbb3c04e3c38609da641cad3afa045e08ca04f4
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-08-19 13:05:47 +0000

    Fix lint issues: remove ptvsd import, fix noqa, fix mutable default, fix env var defaults

--- geoportal/geomapfish_geoportal/resources.py
+++ geoportal/geomapfish_geoportal/resources.py
@@ -5,7 +5,7 @@ from pyramid.security import ALL_PERMISSIONS, Allow
 class Root:
     """The Pyramid root object."""
 
-    __acl__ = [(Allow, "role_admin", ALL_PERMISSIONS)]
+    __acl__ = ((Allow, "role_admin", ALL_PERMISSIONS),)
 
     def __init__(self, request: pyramid.request.Request) -> None:
         self.request = request
