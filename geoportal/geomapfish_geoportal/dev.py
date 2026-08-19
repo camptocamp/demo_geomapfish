d3fe361feeb6f1e4c7e31942dc0a4393b09187ad
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-08-19 14:44:30 +0200

    Fix Ruff rules

--- geoportal/geomapfish_geoportal/dev.py
+++ geoportal/geomapfish_geoportal/dev.py
@@ -29,7 +29,7 @@ def includeme(config: Configurator) -> None:
     del config  # Unused.
 
     try:
-        import ptvsd  # pylint: disable=import-outside-toplevel
+        import ptvsd  # pylint: disable=import-outside-toplevel # noqa: T100
 
         ptvsd.enable_attach(address=("172.17.0.1", 5678))
         # ptvsd.wait_for_attach()
