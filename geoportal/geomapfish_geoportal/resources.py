623f3010bcbeafdce1362d5c6b7dc48e04f9794f
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-08-23 19:05:35 +0000

    Fix lint issues flagged by prospector/ruff

--- geoportal/geomapfish_geoportal/resources.py
+++ geoportal/geomapfish_geoportal/resources.py
@@ -5,7 +5,7 @@ from pyramid.security import ALL_PERMISSIONS, Allow
 class Root:
     """The Pyramid root object."""
 
-    __acl__ = [(Allow, "role_admin", ALL_PERMISSIONS)]
+    __acl__ = [(Allow, "role_admin", ALL_PERMISSIONS)]  # noqa: RUF012
 
     def __init__(self, request: pyramid.request.Request) -> None:
         self.request = request
