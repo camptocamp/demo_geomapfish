912f7265c7e6915193b19cf43aa2b2a1cdfd7a6a
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-08-26 10:08:37 +0000

    Fix prospector lint issues

--- geoportal/geomapfish_geoportal/resources.py
+++ geoportal/geomapfish_geoportal/resources.py
@@ -1,3 +1,5 @@
+import typing
+
 import pyramid.request
 from pyramid.security import ALL_PERMISSIONS, Allow
 
@@ -5,7 +7,7 @@ from pyramid.security import ALL_PERMISSIONS, Allow
 class Root:
     """The Pyramid root object."""
 
-    __acl__ = [(Allow, "role_admin", ALL_PERMISSIONS)]
+    __acl__: typing.ClassVar[list[tuple[str, str, str]]] = [(Allow, "role_admin", ALL_PERMISSIONS)]
 
     def __init__(self, request: pyramid.request.Request) -> None:
         self.request = request
